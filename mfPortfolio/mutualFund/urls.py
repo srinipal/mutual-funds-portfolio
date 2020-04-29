b7c0e4834aaf4d7ab280954af07741bd594ad61b
Srinivas Gundlapalli <savrinis@gmail.com>
2020-04-30 00:15:31 +0530

    MF as a foreign key

--- mfPortfolio/mutualFund/urls.py
+++ mfPortfolio/mutualFund/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
         path('mutual-funds-create/', views.mf_create, name='mfCreate'),
-        path('mutual-funds-view/<int:pk>/', views.MutualFundDetailView.as_view(), name='mfDetail')
+        path('mutual-funds-view/<int:pk>/', views.MutualFundDetailView.as_view(), name='mfDetail'),
+        path('mutual-funds/', views.IndexView.as_view(), name='mfIndex')
     ]
--- mfPortfolio/mutualFund/views.py
+++ mfPortfolio/mutualFund/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from mutualFund.forms import MutualFundForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from mutualFund.models import MutualFund
 
@@ -18,6 +18,15 @@ def mf_create(request):
     form = MutualFundForm()
     return render(request, 'portfolio/create.html', {'form': form})
 
+
 class MutualFundDetailView(DetailView):
     model = MutualFund
     template_name = 'mutualFund/mf_detail.html'
+
+
+class IndexView(ListView):
+    template_name = 'mutualFund/index.html'
+    context_object_name = 'mf_list'
+
+    def get_queryset(self):
+        return MutualFund.objects.all().order_by('mf_name')
